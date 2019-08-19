efd35d1c8fe13c92d267921f78d79a49c1c9b7e8
Mike Chui <mikel.k.khui@gmail.com>
2019-08-19 17:16:36 -0600

    Completed GROUP BY and HAVING exercises

---
+++ 3_queries_group-by/3_asn_per_day.sql
@@ -0,0 +1,15 @@
+
+-- Get the total number of assignments for each day of bootcamp.
+SELECT day, count(*) as total_assignments
+FROM assignments
+GROUP BY day
+ORDER BY day ASC;
+
+
+-- Get the total number of assignments for each day of bootcamp.
+--  but only return rows where total assignments is greater than or equal to 10.
+SELECT day, count(*) as total_assignments
+FROM assignments
+GROUP BY day
+HAVING count(*) >= 10
+ORDER BY day ASC;
---
+++ 3_queries_group-by/3_large_chorts.sql
@@ -0,0 +1,45 @@
+-- Get all cohorts with 18 or more students.
+-- Select the cohort name and the total students.
+-- Order by total students from smallest to greatest.
+SELECT cohorts.name as cphort_name, count(students.*) as student_count
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+HAVING count(students.*) >= 18
+ORDER BY count(students.*) ASC;
+-- 
+-- Get the total number of assignment submissions for each cohort.
+-- Select the cohort name and total submissions.
+-- Order the results from greatest to least submissions.
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+GROUP BY cohorts.name
+ORDER BY count(assignment_submissions.*) DESC;
+-- 
+-- Get currently enrolled students' average assignment completion time.
+-- Select the students name and average time from assignment submissions.
+-- Order the results from greatest duration to least greatest duration.
+-- A student will have a null end_date if they are currently enrolled
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+ORDER BY avg(assignment_submissions.duration) DESC;
+
+-- Get the students who's average time it takes to complete an assignment is less than the average estimated time it takes to complete an assignment.
+-- Select the name of the student, their average completion time, and the average suggested completion time.
+-- Order by average completion time from smallest to largest.
+-- Only get currently enrolled students.
+-- This will require data from students, assignment_submissions, and assignments.
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration,
+avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+JOIN assignments ON assignment_submissions.assignment_id = assignments.id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY avg(assignment_submissions.duration) ASC;
